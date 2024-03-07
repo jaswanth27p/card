4c8411ed1569705ba58d03aeb904c66978a7cfea
jaswanth <jaswanth27p@gmail.com>
2024-03-07 22:38:00 +0530

    card

--- card/src/Card.js
+++ card/src/Card.js
@@ -6,7 +6,7 @@ import { FaRegClock } from "react-icons/fa";
 
 const Card = () => {
   return (
-    <div className="max-w-md mx-auto border border-black bg-blue-100 rounded-xl overflow-hidden shadow-lg relative">
+    <div className="max-w-lg mx-auto border border-black bg-blue-100 rounded-xl overflow-hidden shadow-lg relative transform hover:scale-105 transition-transform">
       <div className="flex flex-col sm:flex-row p-2">
         <div className="sm:w-1/3 pt-4 sm:pt-0 relative ">
           <div className="absolute rounded-full shadow top-8 sm:top-4 right-4 p-2 transition transform hover:scale-110">
@@ -19,9 +19,7 @@ const Card = () => {
           />
         </div>
         <div className="w-full sm:w-2/3 p-4 pr-6">
-          <p
-            className="text-orange-500 text-center w-fit px-2 rounded bg-white text-base font-semibold mb-1"
-          > 
+          <p className="text-orange-500 text-center w-fit px-2 rounded bg-white text-base font-semibold mb-1">
             After Effects
           </p>
           <h2 className="text-3xl font-bold text-blue-700 mb-2">
